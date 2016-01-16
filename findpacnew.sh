abe554ad777f31e3b494f0b3a43624419caec920
Marvin Ewald <marvin.e@protonmail.ch>
2016-01-16 05:36:55 +0100

    Invoke vimdiff on found .pacnew files

--- findpacnew.sh
+++ findpacnew.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-sudo find /etc -name *.pacnew
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    echo -e "Search /etc for .pacnew files and invoke vimdiff on them."
+    echo -e "After each file ask for removal.\n"
+    echo -e "Options:"
+    echo -e "\t-h, --help:\tShow help."
+    echo -e "\t-l, --list:\tOnly list .pacnew files without invoking vimdiff."
+elif [[ "$1" == "-l" ]] || [[ "$1" == "--list" ]]; then
+    find /etc -name *.pacnew
+else
+    dowork () {
+        vimdiff "$(echo $1 | sed s/.pacnew$//)" "$1"
+
+        read -p "Remove $1 ? " -n 1 -r
+        echo
+        [[ $REPLY =~ ^[YyJj]$ ]] && sudo rm "$1"
+    }
+    export -f dowork
+    find /etc -name *.pacnew -execdir bash -c 'dowork {}' \;
+fi
