05a9d2aa175aef27c89795e7a75e339db6d455f0
Marvin Ewald <marvin.e@protonmail.ch>
2016-04-02 18:23:10 +0200

    Allow URLs in hlsound.sh

--- hlsound.sh
+++ hlsound.sh
@@ -14,48 +14,62 @@ removeslashes() {
 
 convertfile() {
     # Strip path and extension from filename and add destination path
-    fname="${1##*/}"
+    local fname
+    if [ -z "$3" ]; then
+        fname="${1##*/}"
+    else
+        fname="$3"
+    fi
     fname="$2/${fname%.*}"
     
-    $DEBUG && (echo "Convert \"$1\" to \"$fname.wav\"" && \
-    echo "Strip metadata and save file to \"$fname-stripped.wav\"" && \
-    echo "Rename \"$fname-stripped.wav\" to \"$fname.wav\"")
-
-    $DEBUG || (ffmpeg -i "$1" -map_metadata -1 -ac 1 -ar 11025 "$fname.wav" && \
-    shntool strip "$fname.wav" && \
-    mv "$fname-stripped.wav" "$fname.wav")
+    if $DEBUG; then
+        echo "Convert '$1' to '$fname.wav'"
+        echo "Strip metadata and save file to '$fname-stripped.wav'"
+        echo "Rename '$fname-stripped.wav' to '$fname.wav'"
+    else
+        ffmpeg -i "$1" -map_metadata -1 -ac 1 -ar 11025 "$fname.wav"
+        shntool strip "$fname.wav"
+        mv "$fname-stripped.wav" "$fname.wav"
+    fi
 }
 
 
 
+
+
+if [ $# -eq 0 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "Converts a file to the half-life 1 sound format."
+    echo -e "Usage:\n\t${0##*/} <files/urls [-o name]>... [-d directory]"
+    echo -e "\nOptions:"
+    echo -e "\t-h, --help\tShow help"
+    echo -e "\t-d\tUse a specific output directory"
+    echo -e "\t-o\tOutput name for the given file/url"
+    exit
+fi
+
 # Default output directory is the working directory
 dest=.
 
-if [ $# -eq 0 ]; then
-    echo "Converts a file to the half-life 1 sound format."
-    echo "Usage:"
-    echo "${0##*/} <files...> [directory]"
-    exit
-else
-    if [ -d "${!#}" ]; then # is the last argument a directory?
-        dest="${!#}"
-        len=$(($#-1))
-    else
-        len=$#
-    fi
-    files=("${@:1:$len}")
+if [ -d "${!#}" ]; then # is the last argument a directory?
+    dest="${!#}"
+    $DEBUG && echo "dest: $dest"
+    $DEBUG && echo "Creating $dest"
+    $DEBUG || mkdir -p "$dest"
 fi
 
-# Remove unnecessary slashes and ensure 1 trailing slash
-# dest="$(removeslashes "$1/")"
-
-$DEBUG && echo "files: ${files[@]}"
-$DEBUG && echo "dest: $dest"
-
-$DEBUG && echo "Create $dest"
-$DEBUG || mkdir -p "$dest"
-
-for i in "${files[@]}"; do
-    convertfile "$i" "$dest"
+while [ $# -gt 1 ]; do
+    file="$1"
+    shift
+    if [ "$1" == '-o' ]; then
+        shift
+        out="$1"
+        shift
+    else
+        out=''
+    fi
+    convertfile "$file" "$dest" "$out"
 done
 
+if [ -n "$1" ] && ! [ -d "$1" ]; then # was the last argument a file?
+    convertfile "$1" "$dest"
+fi
