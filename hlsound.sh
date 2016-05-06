9b63751130203bdf45756f387c85136f00241adb
Marvin Ewald <marvin.e@protonmail.ch>
2016-05-06 11:57:03 +0200

    Rewrite hlsound.sh

--- hlsound.sh
+++ hlsound.sh
@@ -4,93 +4,125 @@
 # Requires ffmpeg, shntool, and optionally youtube-dl #
 #######################################################
 
-# Set DEBUG to true to disable actual conversion and print debug information instead
-DEBUG=${DEBUG:-false}
+printhelp() {
+    echo "(Downloads and) Converts a file to the half-life 1 sound format (suitable for micspam)."
+    echo "If youtube-dl is installed it will be used to extract the video/audio URL from a given link."
 
-
-removeslashes() {
-    echo "$1" | sed s#//*#/#g
-}
-
-convertfile() {
-    local file="$1"
-    local dest="$2"
-    local out="$3"
-
-    if [ -n $youtubedl ] && [[ "$file" =~ $regex ]]; then
-        if [ -z "$out" ]; then
-            if ! out="$($youtubedl -e "$file")"; then
-                out=''
-            fi
-        fi
-
-        if ! file=$($youtubedl -xg "$file"); then
-            file="$1"
-        fi
-    fi
-
-    # Strip path and extension from filename and add destination path
-    local fname
-    if [ -z "$out" ]; then
-        fname="${file##*/}"
-    else
-        fname="$out"
-    fi
-    fname="$dest/${fname%.*}"
-    
-    if $DEBUG; then
-        echo "Convert '$file' to '$fname.wav'"
-        echo "Strip metadata and save file to '$fname-stripped.wav'"
-        echo "Rename '$fname-stripped.wav' to '$fname.wav'"
-    else
-        ffmpeg -i "$file" -map_metadata -1 -ac 1 -ar 11025 "$fname.wav"
-        shntool strip "$fname.wav"
-        mv "$fname-stripped.wav" "$fname.wav"
-    fi
-}
-
-
-if [ $# -eq 0 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "Converts a file to the half-life 1 sound format."
-    echo -e "Usage:\n\t${0##*/} <files/urls [-o name]>... [directory]"
+    echo -e "\nUsage:\n\t${0##*/} <[options] file/url>..."
     echo -e "\nOptions:"
+    echo -e "\tfile/url\tA file or URL to convert. If an URL is given and youtube-dl is available, it will be used to extract the stream URL. If not or if --noytdl is specified, the URL will be downloaded directly."
+    echo -e "\t-d directory\tThe output directory for all files. If it doesn't exist, it will be created. It has no effect if -o is specified."
     echo -e "\t-h, --help\tShow help"
-    echo -e "\t-o\tOutput name for the given file/url"
-    echo -e "\t--noytdl\tDon't use 'youtube-dl -xg <url>' on URLs"
-    exit
-fi
+    echo -e "\t--debug\tDon't do anything, just print what would have been done."
+    echo -e "\t--noytdl\tDon't use youtube-dl to extract the video/audio URL in the given URL."
+    echo -e "\t-o name\tOutput name for the given file/url."
+    echo -e "\t-ss time\tStart position in seconds or in the format 'H:M:S.MS'."
+    echo -e "\t-t duration\tDuration in seconds or in the format 'H:M:S.MS'."
+    echo -e "\t-to time\tEnd position in seconds or in the format 'H:M:S.MS'."
+    echo -e "\nNote: -ss, -t and -to are passed directly to ffmpeg. See also 'man ffmpeg' for further information on these options."
+}
 
-# Default output directory is the working directory
-dest=.
-regex='(https?|ftp)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
-youtubedl=$(command -v youtube-dl >/dev/null 2>&1 && echo 'youtube-dl')
+isurl() {
+    local regex='(https?|ftp)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+    [[ "$1" =~ $regex ]]
+}
 
-if [ -d "${!#}" ]; then # is the last argument a directory?
-    dest="${!#}"
-    $DEBUG && echo "dest: $dest"
-    $DEBUG && echo "Creating $dest"
-    $DEBUG || mkdir -p "$dest"
-fi
-
-while [ $# -gt 1 ]; do
-    if [ "$1" == '--noytdl' ];then
-        youtubedl=''
-        shift
+main() {
+    if [ $# -eq 0 ]; then
+        printhelp
+        exit
     fi
 
-    file="$1"
-    shift
-    if [ "$1" == '-o' ]; then
+    command -v youtube-dl >/dev/null 2>&1 && HAS_YTDL=true || HAS_YTDL=false
+    local NOYTDL=false
+    local INFLAGS=
+    local OUTFLAGS=
+    local OUT=
+    local OUTDIR=.
+    local DEBUG=false
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -h|--help )
+                printhelp
+                exit
+                ;;
+            --debug )
+                DEBUG=true
+                ;;
+            -o )
+                OUT="$2"
+                shift
+                ;;
+            -d )
+                OUTDIR="$2"
+                mkdir -p "$OUTDIR"
+                shift
+                ;;
+            -ss )
+                INFLAGS="-ss $2"
+                shift
+                ;;
+            -t )
+                OUTFLAGS="-t $2"
+                shift
+                ;;
+            -to )
+                OUTFLAGS="-to $2"
+                shift
+                ;;
+            --noytdl )
+                NOYTDL=true
+                ;;
+            * )
+                local FILE="$1"
+
+                if isurl "$FILE" && $HAS_YTDL && ! $NOYTDL; then
+                    IFS=$'\n' 
+                    if local data=($(youtube-dl -xge "$FILE")); then
+                        if [[ -z "$OUT" ]] && [[ ${#data[@]} -gt 1 ]]; then
+                            OUT="$OUTDIR/${data[0]}.wav"
+                        fi
+                        FILE="${data[-1]}"
+                    fi
+                    unset IFS
+                fi
+
+                if [[ -z "$OUT" ]]; then
+                    OUT="$(basename "$FILE")"
+                    OUT="$OUTDIR/${OUT%.*}.wav"
+                fi
+
+                convertfile "$FILE" "$OUT" "$INFLAGS" "$OUTFLAGS"
+
+                # reset
+                NOYTDL=false
+                INFLAGS=
+                OUTFLAGS=
+                OUT=
+                ;;
+        esac
         shift
-        out="$1"
-        shift
+    done
+}
+
+# arg1: input filename
+# arg2: output filename
+# arg3: input flags
+# arg4: output flags
+convertfile() {
+    local OUTFILE="$(mktemp "/tmp/$(basename "$2").XXXXXXXXX.wav")"
+    if $DEBUG; then
+        echo "ffmpeg -y $3 -i \"$1\" $4 -copyts -map_metadata -1 -ac 1 -ar 11025 \"$OUTFILE\""
+        echo "shntool strip \"$OUTFILE\""
+        echo "mv \"${OUTFILE%.wav}-stripped.wav\" \"$2\""
+        echo "rm \"$OUTFILE\""
     else
-        out=''
+        ffmpeg -y $3 -i "$1" $4 -copyts -map_metadata -1 -ac 1 -ar 11025 "$OUTFILE"
+        shntool strip "$OUTFILE"
+        mv "${OUTFILE%.wav}-stripped.wav" "$2"
     fi
+    rm "$OUTFILE"
+}
 
-    convertfile "$file" "$dest" "$out"
-done
-
-if [ -n "$1" ] && ! [ -d "$1" ]; then # was the last argument a file?
-    convertfile "$1" "$dest"
-fi
+main "$@"
