49932c221eac548f5157fa077fab08106f4b4f19
Marvin Ewald <marvin.e@protonmail.ch>
2016-09-25 04:10:47 +0200

    Add option to apply settings only to a given screen

--- scripts/awesome-setbg.sh
+++ scripts/awesome-setbg.sh
@@ -3,17 +3,23 @@
 # Runs the lua command to set the wallpaper.
 # arg1: path to image
 # arg2: type (centered, fit, maximized)
+# arg3: screen (leave blank for all screens)
 set_wallpaper()
 {
-    echo "gears = require('gears')
-          for s = 1, screen.count() do
-              gears.wallpaper.${2:-centered}('$1', s)
-          end" | awesome-client
+    if [[ -z $3 ]]; then
+        echo "gears = require('gears')
+              for s = 1, screen.count() do
+                  gears.wallpaper.${2:-centered}('$1', s)
+              end" | awesome-client
+    else
+        echo "gears = require('gears')
+              gears.wallpaper.${2:-centered}('$1', $3)" | awesome-client
+    fi
 }
 
 printhelp() {
     echo "Set the background in awesome wm."
-    echo -e "Usage:\n\t${0##*/} [options] <filename>"
+    echo -e "Usage:\n\t${0##*/} [options] <filename> [options]"
     echo -e "\nOptions:"
     echo -e "\t-h, --help\tShow help"
     echo -e "\t--apply\tMake the background permanent."
@@ -21,10 +27,12 @@ printhelp() {
     echo -e "\t--fit\tFit the background."
     echo -e "\t--maximize\tMaximize the background."
     echo -e "\t--center\tCenter the background."
+    echo -e "\t-s, --screen <n>\tMake changes only to the nth screen."
 }
 
 main() {
     local FNAME=
+    local SCREEN=
     local APPLY=false
     local TYPE=centered
 
@@ -48,7 +56,10 @@ main() {
                 ;;
             --reset )
                 FNAME=~/.cache/awesome/wallpaper.png
-                break
+                ;;
+            -s|--screen )
+                SCREEN=$2
+                shift
                 ;;
             * )
                 FNAME="$(realpath "$1")"
@@ -57,7 +68,7 @@ main() {
         shift
     done
 
-    [[ -n "$FNAME" ]] && set_wallpaper "$FNAME" $TYPE || exit 1
+    [[ -n "$FNAME" ]] && set_wallpaper "$FNAME" $TYPE $SCREEN || exit 1
 
     if $APPLY; then
         cd ~/.cache/awesome
